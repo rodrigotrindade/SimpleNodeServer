d4d939b9f860957e50fb409f297b708b6b83bfae
Rodrigo Trindade <rodrigo@rodrigotrindade.com.br>
2023-02-10 17:50:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const produtosRoutes = require('./routes/pages')
 app.use(produtosRoutes);
 
 dotenv.config();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!`)
